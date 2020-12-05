45febc5a88493efa2e137054102033078d4444dd
Michael Bedard <bedardm@amazon.com>
2020-12-04 21:32:30 -0500

    day4 working

--- src/com/bart/day4/PassportCheck.java
+++ src/com/bart/day4/PassportCheck.java
@@ -33,8 +33,7 @@ public class PassportCheck {
         for (PassportDetail details : passportList) {
             if (details.validate()) {
                 goodDetails++;
-            } else
-            {
+            } else {
                 badDetails++;
             }
         }
@@ -158,30 +157,25 @@ class PassportDetail {
         if (isBlankString(birthYear) || isBlankString(issueYear) || isBlankString(expirationYear) ||
                 isBlankString(height) || isBlankString(hairColor) || isBlankString(eyeColor) ||
                 isBlankString(passportID)) {
-            // System.out.println("BAD:" + this.toString());
             return false;
         } else if (!checkRanges()) {
-            // System.out.println("BAD:" + this.toString());
             return false;
         }
         return true;
     }
 
     /**
-     *
-     *
      * cid (Country ID) - ignored, missing or not.
      *
      * @return
      */
     private boolean checkRanges() {
-
         // Birth Year
         // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-        int byLength = birthYear.length();
-        if (byLength == 4) {
-            int by = Integer.parseInt(birthYear);
-            if (by <= 1920 && by >= 2002) {
+        int birthLength = birthYear.length();
+        if (birthLength == 4) {
+            int birthInt = Integer.parseInt(birthYear);
+            if (birthInt < 1920 || birthInt > 2002) {
                 return false;
             }
         } else {
@@ -192,8 +186,8 @@ class PassportDetail {
         // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
         int issueLength = issueYear.length();
         if (issueLength == 4) {
-            int by = Integer.parseInt(issueYear);
-            if (by <= 2010 && by >= 2020) {
+            int issueInt = Integer.parseInt(issueYear);
+            if (issueInt < 2010 || issueInt > 2020) {
                 return false;
             }
         } else {
@@ -204,8 +198,8 @@ class PassportDetail {
         // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
         int expLength = expirationYear.length();
         if (expLength == 4) {
-            int by = Integer.parseInt(expirationYear);
-            if (by <= 2020 && by >= 2030) {
+            int expInt = Integer.parseInt(expirationYear);
+            if (expInt < 2020 || expInt > 2030) {
                 return false;
             }
         } else {
@@ -219,21 +213,18 @@ class PassportDetail {
         char ch = height.charAt(0);
         // start with digit
         if (Character.isDigit(ch)) {
-            //System.out.println(height + " starts with a number");
             String[] heightPieces = height.split("(?<=\\d)(?=\\D)");
-            //System.out.println(Arrays.toString(heightPieces));
             // we have two pieces
             if (heightPieces.length == 2) {
                 // validate cm
-                if (heightPieces[1] == "cm") {
-                    int heightCm = Integer.parseInt(heightPieces[1]);
-                    if (heightCm <= 150 && heightCm >= 193) {
+                if (heightPieces[1].equals("cm")) {
+                    int heightCm = Integer.parseInt(heightPieces[0]);
+                    if (heightCm < 150 || heightCm > 193) {
                         return false;
-                    }
-                    // validate in
-                } else if (heightPieces[1] == "in") {
-                    int heightIn = Integer.parseInt(heightPieces[1]);
-                    if (heightIn <= 59 && heightIn >= 76) {
+                    } // validate in
+                } else if (heightPieces[1].equals("in")) {
+                    int heightIn = Integer.parseInt(heightPieces[0]);
+                    if (heightIn < 59 || heightIn > 76) {
                         return false;
                     }
                 }
@@ -243,35 +234,28 @@ class PassportDetail {
         } else {
             return false;
         }
-
 
         // Hair Color
         // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
         char hairCh = hairColor.charAt(0);
         if (hairCh == '#') {
-            // should be six cahracter after the #
+            // should be six characters after the #
             if (hairColor.length() == 7) {
-                //System.out.println(hairColor + " starts with a #");
                 for (int hI = 1; hI < hairColor.length(); hI++) {
-                    //System.out.println("LOOK:" + hairColor.charAt(hI));
                     // skip first one
                     if ((hairColor.charAt(hI) >= '0' && hairColor.charAt(hI) <= '9') ||
                             (hairColor.charAt(hI) >= 'a' && hairColor.charAt(hI) <= 'f')) {
-                        //System.out.println("GOOD");
+                        //nop
                     } else {
-                        //System.out.println("BAD:" + hairColor.charAt(hI));
                         return false;
                     }
                 }
             } else {
-                //System.out.println("BAD-To Long");
                 return false;
             }
         } else {
-            //System.out.println("BAD-No #");
             return false;
         }
-
 
         // Eye Color
         // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
@@ -291,30 +275,21 @@ class PassportDetail {
             case "oth":
                 break;
             default: {
-                //System.out.println("Eye color Not found:"+ eyeColor);
                 return false;
             }
         }
 
         // Passport ID
         // pid (Passport ID) - a nine-digit number, including leading zeroes.
-        if(passportID.length() == 9)
-        {
-            //System.out.println("Try and Format Passport number:"+ passportID);
+        if (passportID.length() == 9) {
             try {
                 Integer.parseInt(passportID);
-                //System.out.println("Its good!");
-            } catch (NumberFormatException ne)
-            {
-                //System.out.println("Passport number NOT a number:"+ passportID);
+            } catch (NumberFormatException ne) {
                 return false;
             }
-        } else
-        {
-            //System.out.println("Passport number too short/long:"+ passportID);
+        } else {
             return false;
         }
-
         return true;
     }
 
